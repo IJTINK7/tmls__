fa80af30a907ce53ebd0732cdddbb616d3c1dede
IJTINK7 <ijtink7@mail.ru>
2024-04-27 22:39:23 +0300

    Getting data from server

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,31 +1,83 @@
+import {useEffect, useState} from "react";
+import {cardsApi} from "../../api/cards-api.ts";
 import styles from "./Card.module.css"
-export const Card = () => {
-	return (
-		<div className={styles.card}>
-			<div className={styles.personalData}>
-				<div className={styles.photo}>
-					<img src="https://pm1.aminoapps.com/7563/896932e3fcd3fe7849f7256a024449829dd2add5r1-300-300v2_uhq.jpg" alt="image"/>
-				</div>
-				<div className={styles.nameAndMail}>
-					<div className={styles.name}>Name Surname</div>
-					<div className={styles.email}>test.mail@gmail.com</div>
-				</div>
-			</div>
-			<div className={styles.additionalData}>
-				<div className={styles.additionalDataContainer}>
-					<div className={styles.phone}>Phone No</div>
-					<div>PhoneInfo</div>
-				</div>
-				<div className={styles.additionalDataContainer}>
-					<div className={styles.birthday}>Birthday</div>
-					<div>BirthdayInfo</div>
-				</div>
-				<div className={styles.additionalDataContainer}>
-					<div className={styles.address}>Address</div>
-					<div>AddressInfo</div>
-				</div>
-			</div>
 
-		</div>
+export  type CardDataType = {
+	dob: {
+		date: string
+	}
+	login: {
+		uuid: string
+	}
+	picture: {
+		large: string
+	}
+	name: {
+		first: string
+		last: string
+	}
+	email: string
+	phone: string
+	location: {
+		city: string
+		state: string
+		country: string
+	}
+}
+
+export type DateOptionsType = {
+	day: 'numeric'
+	month: 'long'
+	year: 'numeric'
+}
+
+export const Card = () => {
+
+	const [state, setState] = useState<CardDataType[]>([])
+	useEffect(() => {
+		cardsApi.getCards()
+			.then((res) => {
+				setState(res.data.results)
+			})
+	}, [])
+	return (
+		<>
+			{state.map((el) => {
+					const timestamp = el.dob.date;
+					const parsedDate = new Date(timestamp);
+					const dateOptions: DateOptionsType = {day: 'numeric', month: 'long', year: 'numeric'};
+					const formattedDate = parsedDate.toLocaleDateString('en-US', dateOptions);
+
+					return (
+						<div className={styles.card} key={el.login.uuid}>
+							<div className={styles.personalData}>
+								<div className={styles.photo}>
+									<img src={el.picture.large} alt="image"/>
+								</div>
+								<div className={styles.nameAndMail}>
+									<div className={styles.name}>{el.name.first} {el.name.last}</div>
+									<div className={styles.email}>{el.email}</div>
+								</div>
+							</div>
+							<div className={styles.additionalData}>
+								<div className={styles.additionalDataContainer}>
+									<div className={styles.phone}>Phone No</div>
+									<div>{el.phone}</div>
+								</div>
+								<div className={styles.additionalDataContainer}>
+									<div className={styles.birthday}>Birthday</div>
+									<div>{formattedDate}</div>
+								</div>
+								<div className={styles.additionalDataContainer}>
+									<div className={styles.address}>Address</div>
+									<div>{el.location.city}, {el.location.state}, {el.location.country}</div>
+								</div>
+							</div>
+
+						</div>
+					)
+				}
+			)}
+		</>
 	);
 };
