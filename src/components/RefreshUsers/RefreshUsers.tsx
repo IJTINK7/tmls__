0c5a0777ca8720353b6e004fb881ddb2f156f7ec
IJTINK7 <ijtink7@mail.ru>
2024-04-28 03:05:50 +0300

    Getting data from server after clicking "Refresh Users" button.

--- src/components/RefreshUsers/RefreshUsers.tsx
+++ src/components/RefreshUsers/RefreshUsers.tsx
@@ -1,7 +1,12 @@
+import {useAppDispatch} from "../../store/store.ts";
+import {getUsersThunkCreator} from "../../store/card-reducer.ts";
 import styles from "./RefreshUsers.module.css"
+
 export const RefreshUsers = () => {
+	const dispatch = useAppDispatch()
+
 	const refreshUsersHandler = () => {
-		// cardsApi.getCards()
+		dispatch(getUsersThunkCreator())
 	}
 	return (
 		<button className={styles.button} onClick={refreshUsersHandler}>Refresh Users</button>
