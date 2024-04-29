f9a8eab477f6394a881f64c813da91afefedbb0e
IJTINK7 <ijtink7@mail.ru>
2024-04-29 06:42:25 +0300

    Adding custom scroll

--- src/components/CardContainer/CardsContainer.tsx
+++ src/components/CardContainer/CardsContainer.tsx
@@ -2,7 +2,7 @@ import {Card} from "../Card/Card.tsx";
 import styles from "./CardsContainer.module.css"
 export const CardsContainer = () => {
 	return (
-		<div className={styles.cardContainer}>
+		<div className={`${styles.cardContainer} ${styles.scroll}`}>
 			<Card/>
 		</div>
 	);
