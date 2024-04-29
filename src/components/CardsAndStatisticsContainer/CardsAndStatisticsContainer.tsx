6aa4703afc6dea038970207f718e565a33f55ca6
IJTINK7 <ijtink7@mail.ru>
2024-04-29 07:40:02 +0300

    Changing scroll styles

--- src/components/CardsAndStatisticsContainer/CardsAndStatisticsContainer.tsx
+++ src/components/CardsAndStatisticsContainer/CardsAndStatisticsContainer.tsx
@@ -1,12 +1,10 @@
 import styles from "./CardsAndStatisticsContainer.module.css"
 import {CardsContainer} from "../CardContainer/CardsContainer.tsx";
 import {StatisticsContainer} from "../StatisticContainer/StatisticsContainer.tsx";
-import {ScrollContainer} from "../ScrollContainer/ScrollContainer.tsx";
 export const CardsAndStatisticsContainer = () => {
 	return (
 		<div className={styles.container}>
 			<div><CardsContainer/></div>
-			<div><ScrollContainer/></div>
 			<div><StatisticsContainer/></div>
 		</div>
 	);
