79692fec61806cf7c56640c7daefe11d4014af73
IJTINK7 <ijtink7@mail.ru>
2024-05-01 21:56:06 +0300

    Adding gradient to CardListContainer

--- src/components/CardListContainer/CardListContainer.tsx
+++ src/components/CardListContainer/CardListContainer.tsx
@@ -1,9 +1,39 @@
 import "./CardListContainer.scss"
 import {CardList} from "../CardList/CardList.tsx";
-export const CardListContainer = () => {
+import {memo, useEffect, useRef, useState} from "react";
+
+export const CardListContainer = memo(() => {
+
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	const handleScroll = () => {
+		const currentRef = ref.current
+		if (currentRef) {
+			const position = currentRef.scrollTop;
+			setScrollPosition(position);
+		}
+	};
+
+	useEffect(() => {
+		const currentRef = ref.current
+		if (currentRef) {
+			currentRef.addEventListener('scroll', handleScroll);
+		}
+		return () => {
+			if (currentRef) {
+				currentRef.removeEventListener('scroll', handleScroll);
+			}
+		};
+	}, []);
+
 	return (
-		<div className="cardListContainer scroll">
-			<CardList/>
+		<div className="cardListContainer scroll" ref={ref}>
+			{scrollPosition !== 0 && <div className="gradient">
+				<div className={"topGradient"}></div>
+				<div className={"bottomGradient"}></div>
+			</div>}
+			<CardList />
 		</div>
 	);
-};
+});
