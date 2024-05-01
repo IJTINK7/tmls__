cc73494549fc899a266ea491b41a1ca314bf1183
IJTINK7 <117001610+IJTINK7@users.noreply.github.com>
2024-05-01 14:21:05 +0300

    Return users count

--- src/api/cards-api.ts
+++ src/api/cards-api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const cardsApi = {
 	getCards() {
-		return axios.get("https://randomuser.me/api/?results=1000");
+		return axios.get("https://randomuser.me/api/?results=500");
 	}
 }
