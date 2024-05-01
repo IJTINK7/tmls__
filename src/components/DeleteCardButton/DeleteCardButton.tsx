b3cb80e3af8c8c2555f2dfd8d24eb2c1dca20daa
IJTINK7 <117001610+IJTINK7@users.noreply.github.com>
2024-05-02 01:47:30 +0300

    Update DeleteCardButton.tsx

--- src/components/DeleteCardButton/DeleteCardButton.tsx
+++ src/components/DeleteCardButton/DeleteCardButton.tsx
@@ -19,7 +19,7 @@ export const DeleteCardButton: React.FC<PropsType> = memo(({isCardActive, cardId
 
 	return isCardActive ? (
 		<button className="deleteCardButton" onClick={deleteCurrentCardHandler}>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256" width="24px" height="24px">
+			<svg xmlns={`${trashIconPath}#trash}`} viewBox="0,0,256,256" width="24px" height="24px">
 				<use xlinkHref={`${trashIconPath}#trash`}/>
 			</svg>
 		</button>
